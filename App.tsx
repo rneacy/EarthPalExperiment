32b86a6b5520c30bb64eb175cf8efe340179dc69
Kirirei <r.neacy@lancaster.ac.uk>
2020-10-15 15:42:02 +0100

    modification into decided app name

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>EarthPal in all it's beautiful glory will go here!</Text>
       <StatusBar style="auto" />
     </View>
   );
