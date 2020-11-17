201803ffe02486e64d9a2817d8c4fa00a483dbcc
Kirirei <r.neacy@lancaster.ac.uk>
2020-11-17 10:15:26 +0000

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Pal from './Objects/Pal'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Pal />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    color: "#FFF",
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Objects/Pal.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// here's ya boi!
-const Pal = () => {
-    return ;
-}
-
-export default Pal;
