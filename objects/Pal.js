97f9b023f8d507370e1b8358ac795944473af1fd
Garry C-F <38925198+GarryC-F@users.noreply.github.com>
2020-11-19 17:44:15 +0000

    Touch stuff

--- objects/Pal.js
+++ objects/Pal.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Text, Image, Dimensions } from 'react-native'
+import { Text, Image, Dimensions, Animated, View } from 'react-native'
+import { Easing } from 'react-native-reanimated';
 import { styles } from '../util/Styles'
+import PopIn from '../objects/PopIn'
+import Idle from '../objects/Idle'
+import Touch from '../objects/Touch'
 
 const valences = {0: "Happy", 1: "Neutral", 2: "Sad"};
 const bestScore = 50;
@@ -28,7 +32,7 @@ const Pal = (props) => {
     if (valenceIndex < 0) valenceIndex = 0
 
     return (
-        <>
+        <Touch>
             <Image
                 source={BaseImages[valenceIndex]["globe"]}
                 style={{width: Dimensions.get('window').width  , height: Dimensions.get('window').width  , position: "absolute"}}
@@ -44,8 +48,10 @@ const Pal = (props) => {
                 style={{width: Dimensions.get('window').width , height: Dimensions.get('window').width  , position: "absolute"}}
             >
             </Image> 
-        </>
+        </Touch>
     )
 }
+
+
 
 export default Pal;
